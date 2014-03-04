53402e1faf7cdd2b93c2bed3d5d5aa44df7d8440
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2014-03-04 21:02:51 +0900

    new file:   Makefile new file:   main.c

---
+++ sixth_pjam/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	int total = 0;
+	int val, i;
+	char buf[32];
+
+	while (fgets(buf, sizeof(buf), stdin) != NULL) {
+
+		val = 0;
+		for (i=0; buf[i]>='0' && buf[i]<='9'; i++) {
+			val *= 10;
+			val += buf[i] - '0';
+		}
+		total += val;
+	}
+
+	printf("%d\n", total);
+
+	return 0;
+}
