af2794faa4fc373a46d4d4eb18bdb05710002b9a
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2014-03-05 08:11:35 +0900

    new file:   Makefile new file:   README new file:   main.c

---
+++ print_args/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	int i;
+
+	printf("%d\n", argc);
+	for (i=0; i<argc; i++) {
+		printf("%p\n", argv[i]);
+		printf("%s\n", argv[i]);
+	}
+	printf("%p\n", argv[i]);
+
+
+	return 0;
+}
