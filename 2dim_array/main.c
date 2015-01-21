35fa1790ffee5cbd45c275a49322afdde66e3a1d
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2015-01-22 00:20:17 +0900

    add 2 main.c

--- 2dim_array/main.c
+++ 2dim_array/main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
+void print_all_names(char name[][32]) {
+	int i;
+
+	for (i=0; i<10; i++) {
+		printf("%2d, %s\n", i, name[i]);
+	}
+	name[0][0]++;
+}
+
 int main(int argc, char **argv) {
 
 	char name[10][32] = {
-		"ABC", "DEFGHI", "JK"
+		"ABC",
+	   	"DEFGHI",
+	   	"JK",
+	   	"",
+	   	"",
+		"ABC",
+	   	"DEFGHI",
+	   	"JK",
+		"",
+		"x"
 	};
 
-	printf("%c\n", name[1][2]);
-	printf("%c\n", name[1][32]);
+	print_all_names(name);
+	print_all_names(name);
 
 	return 0;
 }
