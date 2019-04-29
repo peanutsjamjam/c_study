71412d1a3c3884a96ac4ea2a56cbc82564218b93
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2019-04-29 17:17:58 +0900

    るーぷのしゅうりょうじょうけんをていせい

--- reiwa_or/main.cpp
+++ reiwa_or/main.cpp
@@ -17,13 +17,13 @@ int main(int argc, char **argv) {
 	char str[7];
 
 	printf("二つの年号を or 。\n");
-	for (int i1=0; i1<5; i1++) {
+	for (int i1=0; i1<4; i1++) {
 		for (int i2=i1+1; i2<5; i2++) {
 			printf("%s | %s = %s\n", nengou[i1], nengou[i2], str_or(str, nengou[i1], nengou[i2]));
 		}
 	}
 	printf("\n二つの年号を and 。\n");
-	for (int i1=0; i1<5; i1++) {
+	for (int i1=0; i1<4; i1++) {
 		for (int i2=i1+1; i2<5; i2++) {
 			printf("%s & %s = %s\n", nengou[i1], nengou[i2], str_and(str, nengou[i1], nengou[i2]));
 		}
