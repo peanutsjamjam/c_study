b647af5f10031820ba57e1952d498fd0f139a4a0
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2019-04-29 10:39:27 +0900

    avoid duplicate patterns.

--- reiwa_or/main.cpp
+++ reiwa_or/main.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <cstring>
 
 char *str_or(char *s, const char *s1, const char *s2) {
 	for (int i=0; i<7; i++)
@@ -19,13 +18,13 @@ int main(int argc, char **argv) {
 
 	printf("二つの年号を or 。\n");
 	for (int i1=0; i1<5; i1++) {
-		for (int i2=0; i2<5; i2++) {
+		for (int i2=i1+1; i2<5; i2++) {
 			printf("%s | %s = %s\n", nengou[i1], nengou[i2], str_or(str, nengou[i1], nengou[i2]));
 		}
 	}
 	printf("\n二つの年号を and 。\n");
 	for (int i1=0; i1<5; i1++) {
-		for (int i2=0; i2<5; i2++) {
+		for (int i2=i1+1; i2<5; i2++) {
 			printf("%s & %s = %s\n", nengou[i1], nengou[i2], str_and(str, nengou[i1], nengou[i2]));
 		}
 	}
