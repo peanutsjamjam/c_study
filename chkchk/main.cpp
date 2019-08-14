dcb77b7b91e9178e0c4c4a11ec53e37aa570c656
Atsushi SUGAWARA <peanutsjamjam@gmail.com>
2019-08-14 09:48:40 +0900

    chkchk/

---
+++ chkchk/main.cpp
@@ -0,0 +1,71 @@
+
+#include <cstdio>
+#include <err.h>
+#include <unistd.h>
+
+
+extern int errno;
+static int lineno;
+
+void do_line(const char *buf) {
+	int idx;
+	int col = 0;
+
+	for (idx=0; buf[idx]!='\n' && buf[idx]!=0; idx++) {
+		if (lineno==1 && idx==0) {
+			printf("%02x\n", buf[idx]);
+		}
+	}
+	//printf("%d: %s", lineno, buf);
+}
+
+void do_file(const char *path) {
+	FILE *fp;
+	char buf[2048];
+
+	if ((fp = fopen(path, "r")) == NULL) {
+		errc(1, errno, "%s", path);
+	}
+	lineno = 0;
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		lineno++;
+		do_line(buf);
+	}
+
+	if (ferror(fp)) {
+		errc(1, errno, "%s", path);
+	}
+
+	fclose(fp);
+}
+
+void usage() {
+	fprintf(stderr, "usage:\n");
+	fprintf(stderr, "\tprogram [-b] file...\n");
+}
+
+int main(int argc, char **argv) {
+
+	int ch;
+
+	while ((ch = getopt(argc, argv, "bf:")) != -1) {
+		switch (ch) {
+			case 'b':
+				printf("b\n");
+				break;
+			case 'f':
+				printf("f %s\n", optarg);
+				break;
+			default:
+				usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	for (int i=0; i<argc; i++) {
+		do_file(argv[i]);
+	}
+
+	return 0;
+}
